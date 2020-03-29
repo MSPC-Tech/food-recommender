176ca1c81c4ca49fe600056ec3059880f2bb51da
vipuljain-17 <52670198+vipuljain-17@users.noreply.github.com>
2020-03-29 18:28:14 +0530

    Older verison of api
    The newer version of it has some issue until then use the older one

--- Explore_tab/web/explore_api.py
+++ Explore_tab/web/explore_api.py
@@ -2,47 +2,13 @@ from flask import Flask,jsonify,request,make_response
 from flask_restful import Api,Resource
 import pandas as pd
 
-from pymongo import MongoClient     #To handle mongo db
-
 #Read a tsv file
-df = pd.read_csv("Food Survey_141Responses.tsv",sep = '\t', lineterminator = '\r')
+udf = pd.read_csv("Food Survey_75Responses.tsv",sep = '\t', lineterminator = '\r')
 
 #Manupilating The dataset
 drop_col = ["Timestamp","Food sanctum that doesn't vibes with you"]
-df.drop(drop_col,axis=1,inplace=True)
-df1 = df.iloc[88:,:]
-df1.drop(["Preferred choice"],axis = 1,inplace = True)
-df1.columns = ["What's your name?", 'Year?', 'Select one?','Prefer, North Indian or South Indian?', 'Vegetarian?',
-       'What would you rather prefer?',
-       'Preferred place for Breakfast? (except mess :p)',
-       'Preferred place for Lunch ?(except mess :P)',
-       'Preferred place for Dinner ?(except mess :P)', 'Select One:',
-       'Preferred Chinese Place', 'Preferred Italian Place',
-       'Preferred North Indian Place', 'Preferred South Indian Place',
-       'Preferred Continental Place', 'Tea/Coffee', 'Maggie',
-       'Juices/Milkshakes/Lime', 'Rolls', 'Samosa/Puff', 'Pani Puri/Chat',
-       'Shawarma', 'Sugar Rush', 'Spicy 🌶️', 'Sweet 🍬',
-       'Name the food Sanctum', 'Preferred choice']
-
-df = df.iloc[:88,:]
-df.drop(["Preferred choice.1"],axis = 1,inplace = True)
-df.columns = ["What's your name?", 'Year?', 'Select one?',
-       'Prefer, North Indian or South Indian?', 'Vegetarian?',
-       'What would you rather prefer?',
-       'Preferred place for Breakfast? (except mess :p)',
-       'Preferred place for Lunch ?(except mess :P)',
-       'Preferred place for Dinner ?(except mess :P)', 'Select One:',
-       'Preferred Chinese Place', 'Preferred Italian Place',
-       'Preferred North Indian Place', 'Preferred South Indian Place',
-       'Preferred Continental Place', 'Tea/Coffee', 'Maggie',
-       'Juices/Milkshakes/Lime', 'Rolls', 'Samosa/Puff', 'Pani Puri/Chat',
-       'Shawarma', 'Sugar Rush', 'Preferred choice', 'Spicy 🌶️', 'Sweet 🍬',
-       'Name the food Sanctum']
-
-
-udf = pd.concat([df,df1],ignore_index=True)
-
-cols = ["Name","Year","Hosteller","North-South","Vegetarian","Dine-in","Breakfast","Lunch","Dinner","Cusinies","Chinese","Italain",
+udf.drop(drop_col,axis=1,inplace=True)
+cols = ["Name","Year","Hosteller","North-South","Vegetarin","Dine-in","Breakfast","Lunch","Dinner","Cusinies","Chinese","Italain",
        "North","South","Continental","Tea/Coffee","Maggie","Juices","Rolls","Samosa/Puff","Panipuri","Shawarma","Sugar rush",
        "Spicy-Sweet","Spicy","Sweet","All-time"]
 udf.columns = cols
@@ -50,22 +16,6 @@ udf.columns = cols
 #Create a Flask app(api)
 app = Flask(__name__)
 api = Api(app)
-
-client = MongoClient("mongodb://db:27017")
-db = client.aNewDB
-UserNum = db["UserNum"]
-
-UserNum.insert({
-    'num_of_users' : 0
-})
-
-class Visit(Resource):
-    def get(self):
-        num = UserNum.find({})[0]['num_of_users']
-        num += 1
-        UserNum.update({}, {"$set":{"num_of_users":num}})
-        return str("Hello user " + str(num))
-
 
 @app.route('/')
 def hello_world():
@@ -149,15 +99,15 @@ class South(Resource):
 
 class Continental(Resource):
     def get(self):
-        continental_restro = dict(udf['Continental'].value_counts().sort_values(ascending = False).head(5))
-        a = continental_restro.keys()
+        south_restro = dict(udf['Continental'].value_counts().sort_values(ascending = False).head(5))
+        a = south_restro.keys()
         k= 1
         for i in a:
-            continental_restro[i] = k
+            south_restro[i] = k
             k += 1
         continental_restro = dict([(value, key) for key, value in continental_restro.items()])
         result = {
-            "Continental" :continental_restro
+            "Continental" :south_restro
         }
         return make_response(jsonify(result),200)
 
@@ -169,10 +119,9 @@ api.add_resource(Italain,"/italain")
 api.add_resource(North,"/north")
 api.add_resource(South,"/south")
 api.add_resource(Continental,"/continental")
-api.add_resource(Visit,"/hello")
 
 #for example
 #Run localhost:5000/all-time to get a json result
-#added local host to 0.0.0.0
+
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=False)
