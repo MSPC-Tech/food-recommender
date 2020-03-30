12f0e6b718a62498f55be68531edde9636f34758
Aryan <51026686+Violent-Idiot@users.noreply.github.com>
2020-03-30 13:41:20 +0530

    DB and API tweaks

--- WEB/routes/index.js
+++ WEB/routes/index.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var User = mongoose.model('Users')
+var User = mongoose.model('Users');
+var request = require('request');
 router.get('/', function(req, res, next) {
   res.render("index");
 });
 router.get("/explore", (req,res)=>{
-  res.render("explore");
+  if(req.query.yo_man == null){ var query = "all-time"}
+  else{
+    var query = req.query.yo_man;
+    console.log(query);
+  }
+    
+    var url = "http://localhost:5000/" + query;
+    request(url, (error, response, body)=>{
+    var name = JSON.parse(body);
+    var result = name['Restaurant'];
+    console.log(name);
+    // res.render("explore");
+    res.render("explore", {name : result});
+  });
 });
 
 router.get("/whats", (req,res)=>{
@@ -24,9 +38,11 @@ router.get("/regis", (req,res)=>{
 router.post("/regis", (req,res,next)=>{
   var doc = {
     name : req.body.name,
-    year : req.body.year
+    year : req.body.year,
+    veg : req.body.veg,
+    fav : req.body.fav
   };
-var users = new user(doc);
+var users = new User(doc);
 users.save((error)=>{
     console.log(users);
     if(error){ throw error; }
